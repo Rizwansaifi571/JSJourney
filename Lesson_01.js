8edecbd00e07e3af15d7dcf39b22cb892179dfcd
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-08 23:27:22 +0530

    Just Try.

--- Lesson_01.js
+++ Lesson_01.js
@@ -1,4 +1,27 @@
+// Chapter : Variable And Data.
+// Date : 9, April, 2024.
+
+
+// The value of Js variable can be changed during the execution of a program because Js is an dhynamic language.
+// Example :-
 var a = 67;
 console.log(a);
 var a = "harry";
 console.log(a);
+// variable is determined only during runtime.
+
+// Var, const and let both are used as to determine and identifier.
+
+var a = "this"
+{
+var a =26
+console.log(a)
+}
+console.log(a)
+
+let a = "this"
+{
+let a =26
+console.log(a)
+}
+console.log(a)
