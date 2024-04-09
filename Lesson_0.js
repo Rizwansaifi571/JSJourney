0a3498f1e74509d30f18f8dcfa06eaaa938527e3
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-09 22:53:35 +0530

    Getting_Started.

--- Lesson_0.js
+++ Lesson_0.js
@@ -1,7 +1,10 @@
+// console.log used for print any thing in Java Script.
+
 console.log("hello");
 console.log("354161");
 console.log(13345);
 console.log(465145.61456);
 console.log(641564+465);
 
-// console.log used for print any thing in Java Script.
+// For Comment_out any thing we Use -->  //
+
