32e0fddb86414b65cce281422e96a68644a17122
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-13 19:13:46 +0530

    Variable and Data.

---
+++ Unit_01/lesson_02.js
@@ -0,0 +1,13 @@
+// Lesson : Data.
+// Date : 12, April, 2024.
+
+// Primitive data type : NN BB SS U (trick)
+let a = null ;
+let b = 345 ; 
+let c = true ; 
+let d = BigInt("567") + BigInt("3") ; 
+let e= 'Harry' ; 
+let f = Symbol('Hello') ;
+let g = undefined
+console.log(a, b, c, d , e, f, g)
+console.log(typeof c)
