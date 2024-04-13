648163195c36d2b9d1f5dc84720338f632f45556
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-13 19:36:01 +0530

    Variable and Data.

---
+++ Unit_01/Practise_sheet01.js
@@ -0,0 +1,11 @@
+// Lesson : Practise Question.
+// Date : 13, april, 2024.
+
+// Ques 1 : Create a variable of type string and try to add a number to it.
+
+let a = "Rizwan"
+let b = a + 10
+console.log(a + 10)
+console.log(b)
+
+
