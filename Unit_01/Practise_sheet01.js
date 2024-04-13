95ba1f85852a3b9c56d683a973c8b9981b82d60b
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-13 19:36:46 +0530

    Variable and Data.

--- Unit_01/Practise_sheet01.js
+++ Unit_01/Practise_sheet01.js
@@ -11,4 +11,16 @@ console.log(b)
 // Ques 2 : Use typeof operator to find the data type of the string in last question.
 console.log(typeof(b))
 
-
+// Ques 3 : Creat a const object in Js . can you change it to hold a number later ?
+const market = {
+    item1 : "bed", 
+    price1 : 20000, 
+    item2 : "sofa", 
+    price2 : 14000
+}
+console.log(market["item2"])
+console.log(market["price2"])
+market["item3"] = "chair"
+market["price3"] = 25000
+market["price2"] = 16000
+console.log(market)
