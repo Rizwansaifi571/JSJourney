577ca2198fc6325993723e95665f379006dd227e
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-14 16:34:07 +0530

    Expression and Conditionals.

--- Unit_02/Lesson_01.js
+++ Unit_02/Lesson_01.js
@@ -3,6 +3,8 @@ Lesson : Expression.
 Date : 14, April, 2024.
 */
 
+
+// Arithmetic Operation.
 let a = 10 ;
 let b = 4 ; 
 console.log("a + b = ", a + b)
@@ -19,3 +21,21 @@ console.log("a-- = ", a--)
 console.log("a = ", a)
 console.log("--a = ", --a)
 console.log("a = ", a)
+
+
+// Assignemt Operator.
+let c = 20 ;
+c += 5 ; 
+console.log(c)
+c -= 5 ; 
+console.log(c)
+c *= 2 ;
+console.log(c)
+c /= 2 ; 
+console.log(c)
+c %= 3 ;
+console.log(c) 
+c **= 2 ; 
+console.log(c)
+
+
