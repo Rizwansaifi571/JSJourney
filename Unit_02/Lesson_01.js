6d5b6ad9e00d3738dfa22159d997515185a6a9c5
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-14 16:17:14 +0530

    Expression and Conditionals.

---
+++ Unit_02/Lesson_01.js
@@ -0,0 +1,21 @@
+/* 
+Lesson : Expression.
+Date : 14, April, 2024.
+*/
+
+let a = 10 ;
+let b = 4 ; 
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a ** b = ", a ** b)
+console.log("a / b = ", a / b)
+console.log("a % b = ", a % b)
+console.log("a++ = ", a++)
+console.log("a = ", a)
+console.log("++a = ", ++a)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("--a = ", --a)
+console.log("a = ", a)
