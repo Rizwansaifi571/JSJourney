b3e8b312e5ecc5989b05f0b8e804f22e038f3d6f
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-21 21:21:35 +0530

    String.

---
+++ Unit_04/Lesson_02.js
@@ -0,0 +1,11 @@
+/*
+Lesson - String Function.
+Date : 21, April, 2024.
+*/
+
+// Example 1 : 
+
+let name1  = "Harry"
+console.log(name1.length)
+console.log(name1.toUpperCase())
+console.log(name1.toLowerCase())
