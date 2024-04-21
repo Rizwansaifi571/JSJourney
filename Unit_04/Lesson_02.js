1c9729e4ef6fa7c450425a31526fe57c96b0264b
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-21 21:23:54 +0530

    String.

--- Unit_04/Lesson_02.js
+++ Unit_04/Lesson_02.js
@@ -3,9 +3,17 @@ Lesson - String Function.
 Date : 21, April, 2024.
 */
 
-// Example 1 : 
+// Example 1 : Basic Function.
 
 let name1  = "Harry"
 console.log(name1.length)
 console.log(name1.toUpperCase())
 console.log(name1.toLowerCase())
+
+
+// Example 2 : Slicing of string.
+ 
+name1 = "Harry"
+console.log(name1.slice(2, 4))
+console.log(name1.slice(1, 4))
+console.log(name1.slice(2))
