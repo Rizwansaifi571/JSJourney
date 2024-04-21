3f1b34c49b16cf51015d6258a6b6f5c424ee0bb6
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-21 21:28:40 +0530

    String.

--- Unit_04/Lesson_02.js
+++ Unit_04/Lesson_02.js
@@ -17,3 +17,11 @@ name1 = "Harry"
 console.log(name1.slice(2, 4))
 console.log(name1.slice(1, 4))
 console.log(name1.slice(2))
+
+
+// Example 3 : More functions
+
+name1 = "Harry Bhai"
+let newname = name1.replace("Bhai", "Bhau")
+console.log(newname)
+
