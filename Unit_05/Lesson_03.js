d5accdff6af3d2deadc14b6f68f5e454866af6e9
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-27 13:11:34 +0530

    Arrays.

---
+++ Unit_05/Lesson_03.js
@@ -0,0 +1,10 @@
+/*
+Lesson - Looping Through Arrays.
+Date : 27, April, 2024.
+*/
+
+// Classical For Loop.
+let num = [3, 5, 1, 2, 4] 
+for (let i = 0 ; i<num.length ; i++){
+    console.log(num[i])
+}
