0c345d09503a3e3841f8e3ddba0c974171b11ace
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-04-27 13:14:52 +0530

    Arrays.

--- Unit_05/Lesson_03.js
+++ Unit_05/Lesson_03.js
@@ -8,3 +8,10 @@ let num = [3, 5, 1, 2, 4]
 for (let i = 0 ; i<num.length ; i++){
     console.log(num[i])
 }
+
+
+// For Each Loop.
+num = [3, 5, 1, 2, 4] 
+num.forEach((element) => {
+    console.log(element)
+})
